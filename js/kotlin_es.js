101085344ba8f892388a66a3f4a2a77ae2e9b549
Carlos Ballesteros Velasco <soywiz@gmail.com>
2015-11-21 12:41:51 +0100

    Fijada barra de arriba con efectos de transición

--- js/kotlin_es.js
+++ js/kotlin_es.js
@@ -1,3 +1,16 @@
 $(function() {
-    $('#')
+    //$('a[href^="http://"]').attr('target','_blank');
+
+    var fixContent = $("#main-navbar");
+
+    var minOffset = fixContent.offset().top;
+
+    $(document).on("scroll", function(e) {
+      if ($(document).scrollTop() > minOffset) {
+        fixContent.addClass("fix-top");
+      } else {
+        fixContent.removeClass("fix-top");
+      }
+
+    });
 });
